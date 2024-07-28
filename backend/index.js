b30291083daee0a43ab92d10bd237aefd7487b99
pammu453 <pramodsavant2001@gmail.com>
2024-07-28 19:12:13 +0530

    Code refactered

--- backend/index.js
+++ backend/index.js
@@ -14,12 +14,6 @@ app.use(bodyParser.json());
 const chatRoutes = require('./routes/chat.js');
 app.use('/api/chat', chatRoutes);
 
-// app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-// });
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
